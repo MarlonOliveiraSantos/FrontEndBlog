bf45d2e1fcdcc6817aee28f5c3a9565b5d61c378
marlon <marlon.oliveira34@hotmail.com>
2023-11-07 17:20:27 -0300

    commit referente home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-
 
 function Home() {
+
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
@@ -9,6 +8,7 @@ function Home() {
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
                             Seja Bem Vinde!
+                            <h2 className='text-5xl font-bold'>Seja bem vinde, { }!</h2>
                         </h2>
                         <p className='text-xl'>
                             Expresse aqui seus pensamentos e opniões
@@ -33,7 +33,9 @@ function Home() {
                 </div>
             </div>
         </>
+        
     )
+    
 }
 
 export default Home
